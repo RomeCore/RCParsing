aaa3d621d9ca67c75ed48e97f97961e0a4e35b4c
RomeCore <62770895+RomeCore@users.noreply.github.com>
2025-09-27 02:47:57 +0500

    Add C parser to tests

---
+++ tests/RCParsing.Tests/CGrammarTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RCParsing.Tests.C;
+
+namespace RCParsing.Tests
+{
+	public class CGrammarTests
+	{
+		[Fact]
+		public void SimpleC()
+		{
+			string input =
+			"""
+			int func(double) {
+				return 0.0;
+			}
+
+			void main(int) {
+				return 2 + 2;
+			}
+			""";
+
+			var parser = CParser.CreateParser();
+
+			var ast = parser.Parse(input);
+		}
+	}
+}
